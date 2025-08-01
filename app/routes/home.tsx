745180fec6ab92d0afb157675a729fadee7e9619
vutu266 <vutulsss@gmail.com>
2025-08-01 16:33:29 +0700

    adding fake data

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,7 @@
-import { resumes } from "constants";
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import ResumeCard from "~/components/ResumeCard";
+import { resumes } from "constants/index";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -16,19 +16,19 @@ export default function Home() {
 
     <Navbar />
     <section className="main-section">
-      <div className="page-heading">
-        <h1>Track your Applications & Resume Rating</h1>
-        <h2>Review your submissions and check AI-powered feedback.</h2>
-      </div>
-    </section>
+        <div className="page-heading py-16">
+          <h1>Track your Applications & Resume Rating</h1>
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        </div>
 
-    {resumes.length > 0 && 
-    <div className="resumes-section">
-      {resumes.map((resume: any) => (
-          <ResumeCard key={resumes.id} resume={resume}/>
-      ))}
-    </div>
-    }
+      {resumes.length > 0 &&
+      <div className="resumes-section">
+        {resumes.map((resume: any) => (
+          <ResumeCard key={resume.id} resume={resume}/>
+        ))}
+      </div>
+      }
+    </section>
     
   </main>
 
--- constants/index.ts
+++ constants/index.ts
@@ -3,7 +3,7 @@ export const resumes: Resume[] = [
     id: "1",
     companyName: "Google",
     jobTitle: "Frontend Developer",
-    imagePath: "/images/resume-1.png",
+    imagePath: "/images/resume_01.png",
     resumePath: "/resumes/resume-1.pdf",
     feedback: {
       overallScore: 85,
@@ -33,7 +33,7 @@ export const resumes: Resume[] = [
     id: "2",
     companyName: "Microsoft",
     jobTitle: "Cloud Engineer",
-    imagePath: "/images/resume-2.png",
+    imagePath: "/images/resume_02.png",
     resumePath: "/resumes/resume-2.pdf",
     feedback: {
       overallScore: 55,
@@ -63,7 +63,97 @@ export const resumes: Resume[] = [
     id: "3",
     companyName: "Apple",
     jobTitle: "iOS Developer",
-    imagePath: "/images/resume-3.png",
+    imagePath: "/images/resume_03.png",
+    resumePath: "/resumes/resume-3.pdf",
+    feedback: {
+      overallScore: 75,
+      ATS: {
+        score: 90,
+        tips: [],
+      },
+      toneAndStyle: {
+        score: 90,
+        tips: [],
+      },
+      content: {
+        score: 90,
+        tips: [],
+      },
+      structure: {
+        score: 90,
+        tips: [],
+      },
+      skills: {
+        score: 90,
+        tips: [],
+      },
+    },
+  },
+   {
+    id: "4",
+    companyName: "Google",
+    jobTitle: "Frontend Developer",
+    imagePath: "/images/resume_01.png",
+    resumePath: "/resumes/resume-1.pdf",
+    feedback: {
+      overallScore: 85,
+      ATS: {
+        score: 90,
+        tips: [],
+      },
+      toneAndStyle: {
+        score: 90,
+        tips: [],
+      },
+      content: {
+        score: 90,
+        tips: [],
+      },
+      structure: {
+        score: 90,
+        tips: [],
+      },
+      skills: {
+        score: 90,
+        tips: [],
+      },
+    },
+  },
+  {
+    id: "5",
+    companyName: "Microsoft",
+    jobTitle: "Cloud Engineer",
+    imagePath: "/images/resume_02.png",
+    resumePath: "/resumes/resume-2.pdf",
+    feedback: {
+      overallScore: 55,
+      ATS: {
+        score: 90,
+        tips: [],
+      },
+      toneAndStyle: {
+        score: 90,
+        tips: [],
+      },
+      content: {
+        score: 90,
+        tips: [],
+      },
+      structure: {
+        score: 90,
+        tips: [],
+      },
+      skills: {
+        score: 90,
+        tips: [],
+      },
+    },
+  },
+  {
+    id: "6",
+    companyName: "Apple",
+    jobTitle: "iOS Developer",
+    imagePath: "/images/resume_03.png",
     resumePath: "/resumes/resume-3.pdf",
     feedback: {
       overallScore: 75,
